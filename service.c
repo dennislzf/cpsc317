8b83cc07f45caf345382926cd59bb14b08cb0d22
alex <Alexanderjettel@gmail.com>
2012-12-01 15:51:54 -0800

    Receive working. Dynamic arrays

--- service.c
+++ service.c
@@ -18,19 +18,85 @@
 #include "service.h"
 #include "util.h"
 
-char receive_buffer[3000];
+int commandBufferCapcity = 100;
+int commandBufferContent = 0;
 
 void handle_client(int socket) {
+    char *commandBuffer = malloc(sizeof(char) * commandBufferCapcity);
 
-    /* TODO Loop receiving requests and sending appropriate responses,
-     *      until one of the conditions to close the connection is
-     *      met.
-     */
+
     while(1){
-        //receive();
+        receive(socket,commandBuffer);
         //parseCommand();
         //buildResponse();
         //sendResponse();
+        flushCommandBuffer();
+        printf("\nDone\n");
+        break;
     }
     return;
+}
+
+/*
+ * Receives a command from a socket
+ * and writes it into the commandBuffer
+ *
+ * We fist read it into a small local buffer and then copy it over
+ * If the command buffer is too small, we are resizing
+ */
+void receive(int socket,char* commandBuffer){
+    int receiveBufferSize = 100;
+    int currentBufferCopyPos = 0;
+    char receive_buffer[receiveBufferSize];
+
+    int receivedBytes;
+    do{
+        // Read 'receiveBufferSize' chars into the buffer
+        receivedBytes = recv(socket, receive_buffer, receiveBufferSize,0);
+
+
+        // Check if we need to resize the command array
+        if((receivedBytes + currentBufferCopyPos) >= commandBufferCapcity){
+            printf("Resizing\n");
+            commandBuffer = resizeCommandArray(commandBuffer);
+        }
+
+        // Copy the received bytes into the command buffer
+        int i;
+        for(i = 0; i < receivedBytes;i++){
+            commandBuffer[currentBufferCopyPos] = receive_buffer[i];
+            currentBufferCopyPos += 1;
+        }
+
+        commandBufferContent = currentBufferCopyPos;
+    }
+    while(receivedBytes > 0); // while there is data to read
+
+
+}
+/*
+ * This resizes the command array and copies the old elements over
+ */
+char * resizeCommandArray(char* commandBuffer){
+    // Create a new buffer
+    commandBufferCapcity = commandBufferCapcity * 2;
+    char *newCommandBuffer = malloc(sizeof(char) * commandBufferCapcity);
+
+    // Copy the old elements over
+    int index;
+    for(index = 0; index < commandBufferContent;index++){
+        newCommandBuffer[index] = commandBuffer[index];
+    }
+
+    // Delete the old command Buffer
+    free(commandBuffer);
+
+    return newCommandBuffer;
+}
+/*
+ * Resets the position of the command buffer content to 0
+ * so we can reuse it
+ */
+void flushCommandBuffer(){
+    commandBufferContent = 0;
 }
--- service.h
+++ service.h
@@ -6,5 +6,7 @@
 #define _SERVICE_H_
 
 void handle_client(int socket);
-
+void receive(int socket, char* commandBuffer);
+char * resizeCommandArray(char* commandBuffer);
+void flushCommandBuffer();
 #endif
